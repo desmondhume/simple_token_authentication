4d7eb6e6683424f55789e439dcd6979d3f4bc1e2
Gregorio Setti <gregorio.setti@gmail.com>
2014-11-04 16:24:43 +0100

    Update rails_adapter.rb

--- lib/simple_token_authentication/adapters/rails_adapter.rb
+++ lib/simple_token_authentication/adapters/rails_adapter.rb
@@ -7,7 +7,7 @@ module SimpleTokenAuthentication
       extend SimpleTokenAuthentication::Adapter
 
       def self.base_class
-        ::ActionController::Base
+        ::ActionController::API
       end
     end
   end
